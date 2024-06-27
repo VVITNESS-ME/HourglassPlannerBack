aa5495a2f8aa001c399baf8723c97d39e5f4dea4
PenLoo98 <sohnchanho@gmail.com>
2024-06-27 21:19:53 +0900

    Initial commit: Create project

---
+++ src/test/java/com/myweapon/hourglass/HourglassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myweapon.hourglass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HourglassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
