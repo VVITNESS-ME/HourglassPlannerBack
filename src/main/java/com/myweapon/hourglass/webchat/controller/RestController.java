453c050c80bda6cf87ed351725371332096d8e6a
PenLoo98 <sohnchanho@gmail.com>
2024-07-09 15:42:35 +0900

    feat: add Check Status in EC2 Target Group

--- src/main/java/com/myweapon/hourglass/webchat/controller/RestController.java
+++ src/main/java/com/myweapon/hourglass/webchat/controller/RestController.java
@@ -1,5 +1,6 @@
 package com.myweapon.hourglass.webchat.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,5 +10,11 @@ public class RestController {
     public ResponseEntity<String> hello() {
         ResponseEntity<String> response = ResponseEntity.ok("Hello, World!");
         return response;
+    }
+
+    @GetMapping("/health-check")
+    public ResponseEntity<Void> checkHealthStatus() {
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
