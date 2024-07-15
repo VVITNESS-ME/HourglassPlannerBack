022ac16aa72e2cbe2157a0dea15644bd23d8fe2b
Kyo Jun Hwang <hwangkyojun7806@gmail.com>
2024-07-15 22:21:42 +0900

    Fix : remove secrete key

--- src/main/java/com/myweapon/hourglass/common/api/config/RestApiConfig.java
+++ src/main/java/com/myweapon/hourglass/common/api/config/RestApiConfig.java
@@ -1,6 +1,5 @@
 package com.myweapon.hourglass.common.api.config;
 
-import com.myweapon.hourglass.common.api.service.AuthenticationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
--- src/main/java/com/myweapon/hourglass/common/api/service/AuthenticationService.java
+++ src/main/java/com/myweapon/hourglass/common/api/service/AuthenticationService.java
@@ -2,11 +2,11 @@ package com.myweapon.hourglass.common.api.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class AuthenticationService {
-    private String GPTAuthToken = "sk-proj-EEmpNHijWevsQi4XtqAJT3BlbkFJDYcRmbAXICofoIJFR7Et";
-
-    public String getChatGPTAuthToken() {
-        return GPTAuthToken;
-    }
-}
+//@Service
+//public class AuthenticationService {
+//
+//
+//    public String getChatGPTAuthToken() {
+//        return GPTAuthToken;
+//    }
+//}
